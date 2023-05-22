70c4039f5e3bfbac701e451d2141390fe7fcc4f1
1 <margoloko@mail.ru>
2023-05-22 15:16:58 +0700

    fix pep8

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 google_api_project
+
 ![example workflow](https://github.com/margoloko/google_api_project/actions/workflows/main.yaml/badge.svg)
 ## Изучение Google Cloud Platform
 ### Приложения для контроля бюджета путешествий с формированием отчёта в гугл-таблицах
--- travel_budget/services.py
+++ travel_budget/services.py
@@ -12,16 +12,16 @@ SCOPES = [
 load_dotenv()
 EMAIL_USER = os.environ['EMAIL']
 INFO = {
-    'type':  os.environ['TYPE'],
-    'project_id':  os.environ['PROJECT_ID'],
-    'private_key_id':  os.environ['PRIVATE_KEY_ID'],
-    'private_key':  os.environ['PRIVATE_KEY'],
-    'client_email':  os.environ['CLIENT_EMAIL'],
-    'client_id':  os.environ['CLIENT_ID'],
-    'auth_uri':  os.environ['AUTH_URI'],
-    'token_uri':  os.environ['TOKEN_URI'],
-    'auth_provider_x509_cert_url':  os.environ['AUTH_PROVIDER_X509_CERT_URL'],
-    'client_x509_cert_url':  os.environ['CLIENT_X509_CERT_URL']
+    'type': os.environ['TYPE'],
+    'project_id': os.environ['PROJECT_ID'],
+    'private_key_id': os.environ['PRIVATE_KEY_ID'],
+    'private_key': os.environ['PRIVATE_KEY'],
+    'client_email': os.environ['CLIENT_EMAIL'],
+    'client_id': os.environ['CLIENT_ID'],
+    'auth_uri': os.environ['AUTH_URI'],
+    'token_uri': os.environ['TOKEN_URI'],
+    'auth_provider_x509_cert_url': os.environ['AUTH_PROVIDER_X509_CERT_URL'],
+    'client_x509_cert_url': os.environ['CLIENT_X509_CERT_URL']
 }
 CREDENTIALS = Credentials.from_service_account_info(info=INFO,
                                                     scopes=SCOPES)
