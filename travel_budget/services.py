571e6275cefac189ffd5b2041eb24bec7414d6be
1 <margoloko@mail.ru>
2023-05-23 15:42:16 +0700

    add logging

--- travel_budget/services.py
+++ travel_budget/services.py
@@ -34,6 +34,7 @@ def get_logger():
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
     handler = logging.StreamHandler()
-    handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    handler.setFormatter(logging.Formatter('%(asctime)s - '
+                                           '%(levelname)s - %(message)s'))
     logger.addHandler(handler)
     return logger
