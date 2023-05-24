7119670ded8919b06b22a9cae911847347553d4e
1 <margoloko@mail.ru>
2023-05-24 14:42:37 +0700

    add black

--- control_drive.py
+++ control_drive.py
@@ -7,44 +7,47 @@ from pprint import pprint
 
 
 SCOPES = [
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive'
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
 ]
 
 load_dotenv()
 
 info = {
-    'type':  os.environ['TYPE'],
-    'project_id':  os.environ['PROJECT_ID'],
-    'private_key_id':  os.environ['PRIVATE_KEY_ID'],
-    'private_key':  os.environ['PRIVATE_KEY'],
-    'client_email':  os.environ['CLIENT_EMAIL'],
-    'client_id':  os.environ['CLIENT_ID'],
-    'auth_uri':  os.environ['AUTH_URI'],
-    'token_uri':  os.environ['TOKEN_URI'],
-    'auth_provider_x509_cert_url':  os.environ['AUTH_PROVIDER_X509_CERT_URL'],
-    'client_x509_cert_url':  os.environ['CLIENT_X509_CERT_URL']
+    "type": os.environ["TYPE"],
+    "project_id": os.environ["PROJECT_ID"],
+    "private_key_id": os.environ["PRIVATE_KEY_ID"],
+    "private_key": os.environ["PRIVATE_KEY"],
+    "client_email": os.environ["CLIENT_EMAIL"],
+    "client_id": os.environ["CLIENT_ID"],
+    "auth_uri": os.environ["AUTH_URI"],
+    "token_uri": os.environ["TOKEN_URI"],
+    "auth_provider_x509_cert_url": os.environ["AUTH_PROVIDER_X509_CERT_URL"],
+    "client_x509_cert_url": os.environ["CLIENT_X509_CERT_URL"],
 }
+
 
 # Функция авторизации.
 def auth():
     credentials = Credentials.from_service_account_info(info=info, scopes=SCOPES)
-    service = discovery.build('drive', 'v3', credentials=credentials)
+    service = discovery.build("drive", "v3", credentials=credentials)
     return service
 
 
 def get_list_obj(service):
-    response = service.files().list(q='mimeType="application/vnd.google-apps.spreadsheet"')
+    response = service.files().list(
+        q='mimeType="application/vnd.google-apps.spreadsheet"'
+    )
     return response.execute()
 
 
 def clear_disk(service, spreadsheets):
     """Функция удаляет все документы с таблицами на гугл-диске."""
     for spreadsheet in spreadsheets:
-        response = service.files().delete(fileId=spreadsheet['id']).execute()
+        response = service.files().delete(fileId=spreadsheet["id"]).execute()
 
 
 service = auth()
-spreadsheets = get_list_obj(service)['files']
+spreadsheets = get_list_obj(service)["files"]
 pprint(get_list_obj(service))
-#clear_disk(service, spreadsheets)
+# clear_disk(service, spreadsheets)
--- training_spreadsheets.py
+++ training_spreadsheets.py
@@ -5,98 +5,101 @@ from google.oauth2.service_account import Credentials
 from googleapiclient import discovery
 
 
-
-
 load_dotenv()
 
-EMAIL_USER = os.environ['EMAIL']
+EMAIL_USER = os.environ["EMAIL"]
 SCOPES = [
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive']
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive",
+]
 
 info = {
-    'type':  os.environ['TYPE'],
-    'project_id':  os.environ['PROJECT_ID'],
-    'private_key_id':  os.environ['PRIVATE_KEY_ID'],
-    'private_key':  os.environ['PRIVATE_KEY'],
-    'client_email':  os.environ['CLIENT_EMAIL'],
-    'client_id':  os.environ['CLIENT_ID'],
-    'auth_uri':  os.environ['AUTH_URI'],
-    'token_uri':  os.environ['TOKEN_URI'],
-    'auth_provider_x509_cert_url':  os.environ['AUTH_PROVIDER_X509_CERT_URL'],
-    'client_x509_cert_url':  os.environ['CLIENT_X509_CERT_URL']
+    "type": os.environ["TYPE"],
+    "project_id": os.environ["PROJECT_ID"],
+    "private_key_id": os.environ["PRIVATE_KEY_ID"],
+    "private_key": os.environ["PRIVATE_KEY"],
+    "client_email": os.environ["CLIENT_EMAIL"],
+    "client_id": os.environ["CLIENT_ID"],
+    "auth_uri": os.environ["AUTH_URI"],
+    "token_uri": os.environ["TOKEN_URI"],
+    "auth_provider_x509_cert_url": os.environ["AUTH_PROVIDER_X509_CERT_URL"],
+    "client_x509_cert_url": os.environ["CLIENT_X509_CERT_URL"],
 }
+
 
 def auth():
     """Функция авторизации."""
-    credentials = Credentials.from_service_account_info(
-                  info=info, scopes=SCOPES)
+    credentials = Credentials.from_service_account_info(info=info, scopes=SCOPES)
     # Создаём экземпляр класса Resource.
-    service = discovery.build('sheets', 'v4', credentials=credentials)
+    service = discovery.build("sheets", "v4", credentials=credentials)
     return service, credentials
 
 
 def create_spreadsheet(service):
     """Функция создания документа."""
-    spreadsheet_body = {'properties': { # Свойства документа
-                        'title': 'Бюджет путешествий',
-                        'locale': 'ru_RU' },
-                        # Свойства листов документа
-                        'sheets': [{
-                            'properties': {
-                               'sheetType': 'GRID',
-                               'sheetId': 0,
-                               'title': 'Отпуск 2077',
-                               'gridProperties': {
-                                    'rowCount': 100,
-                                    'columnCount': 100}
-                                             }
-                        }]}
+    spreadsheet_body = {
+        "properties": {  # Свойства документа
+            "title": "Бюджет путешествий",
+            "locale": "ru_RU",
+        },
+        # Свойства листов документа
+        "sheets": [
+            {
+                "properties": {
+                    "sheetType": "GRID",
+                    "sheetId": 0,
+                    "title": "Отпуск 2077",
+                    "gridProperties": {"rowCount": 100, "columnCount": 100},
+                }
+            }
+        ],
+    }
     request = service.spreadsheets().create(body=spreadsheet_body)
     response = request.execute()
-    spreadsheet_id = response['spreadsheetId']
-    print('https://docs.google.com/spreadsheets/d/' + spreadsheet_id)
+    spreadsheet_id = response["spreadsheetId"]
+    print("https://docs.google.com/spreadsheets/d/" + spreadsheet_id)
     return spreadsheet_id
 
 
 def set_user_permissions(spreadsheet_id, credentials):
-    permissions_body={'type': 'user', # Тип учетных данных.
-                      'role': 'writer', # Права доступа для учётной записи.
-                      'emailAddress': EMAIL_USER} # Ваш личный гугл-аккаунт.
+    permissions_body = {
+        "type": "user",  # Тип учетных данных.
+        "role": "writer",  # Права доступа для учётной записи.
+        "emailAddress": EMAIL_USER,
+    }  # Ваш личный гугл-аккаунт.
 
     # Создаётся экземпляр класса Resource для Google Drive API.
-    drive_service = discovery.build('drive', 'v3', credentials=credentials)
+    drive_service = discovery.build("drive", "v3", credentials=credentials)
 
     # Формируется и сразу выполняется запрос на выдачу прав вашему аккаунту.
     drive_service.permissions().create(
-        fileId=spreadsheet_id,
-        body=permissions_body,
-        fields='id'
+        fileId=spreadsheet_id, body=permissions_body, fields="id"
     ).execute()
 
 
 def spreadsheet_update_values(service, spreadsheetId):
     table_values = [
-        ['Бюджет путешествий'],
-        ['Весь бюджет', '5000'],
-        ['Все расходы', '=SUM(E7:E30)'],
-        ['Остаток', '=B2-B3'],
-        ['Расходы'],
-        ['Описание', 'Тип', 'Кол-во', 'Цена', 'Стоимость'],
-        ['Перелет', 'Транспорт', '2', '400', '=C7*D7']
+        ["Бюджет путешествий"],
+        ["Весь бюджет", "5000"],
+        ["Все расходы", "=SUM(E7:E30)"],
+        ["Остаток", "=B2-B3"],
+        ["Расходы"],
+        ["Описание", "Тип", "Кол-во", "Цена", "Стоимость"],
+        ["Перелет", "Транспорт", "2", "400", "=C7*D7"],
     ]
 
     # Тело запроса.
-    request_body = {
-        'majorDimension': 'ROWS',
-        'values': table_values
-    }
+    request_body = {"majorDimension": "ROWS", "values": table_values}
     # Формирование запроса к Google Sheets API.
-    request = service.spreadsheets().values().update(
-        spreadsheetId=spreadsheetId,
-        range='Отпуск 2077!A1:F20',
-        valueInputOption='USER_ENTERED',
-        body=request_body
+    request = (
+        service.spreadsheets()
+        .values()
+        .update(
+            spreadsheetId=spreadsheetId,
+            range="Отпуск 2077!A1:F20",
+            valueInputOption="USER_ENTERED",
+            body=request_body,
+        )
     )
     # Выполнение запроса.
     request.execute()
@@ -104,10 +107,13 @@ def spreadsheet_update_values(service, spreadsheetId):
 
 # Напишите функцию, которая получит данные из диапазона Лист1!A1:I10
 def read_values(service, spreadsheet_id):
-    result = service.spreadsheets().values().get(
-        spreadsheetId=spreadsheet_id,
-        range='Лист1!A1:F10').execute()
-    return result['values']
+    result = (
+        service.spreadsheets()
+        .values()
+        .get(spreadsheetId=spreadsheet_id, range="Лист1!A1:F10")
+        .execute()
+    )
+    return result["values"]
 
 
 # Вызов функций
